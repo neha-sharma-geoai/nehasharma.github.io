4fb40ce9ff54f97644249c79f3662169161bbd09
neha-sharma-geoai <125634000+neha-sharma-geoai@users.noreply.github.com>
2025-07-20 15:45:09 +0000

    Deploying to gh-pages from @ neha-sharma-geoai/neha-sharma-geoai.github.io@ba8a62324f8e501c1939958968afeace0ce8557a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -550,9 +550,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-coffee-kiosk-project-walkthrough",
+          title: 'Coffee Kiosk Project Walkthrough',
+          description: "Walkthrough Projects",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-lakeview-apartments-project-walkthrough",
