102579c6fe948ca076b447a2424d1cae64f8bcb4
neha-sharma-geoai <125634000+neha-sharma-geoai@users.noreply.github.com>
2025-07-27 22:34:32 +0000

    Deploying to gh-pages from @ neha-sharma-geoai/neha-sharma-geoai.github.io@6d1c7bc3f772424154f8f6c13139d3b3cca229a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -580,7 +580,7 @@ ninja.data = [{
               window.location.href = "/projects/5_project/";
             },},{id: "projects-fine-grained-roof-type-classification-amp-multimodal-multitasking-building-detection-amp-height-estimation",
           title: 'Fine-Grained Roof Type Classification &amp;amp; Multimodal Multitasking Building Detection &amp;amp; Height Estimation',
-          description: "MS Data Science Thesis",
+          description: "MS Data Science Thesis (GeoAI)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-assessment-of-cropping-patterns-in-districts-of-gujarat",
