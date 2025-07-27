e510fed8242b1dd6cfab657ba60fde4f8c9ded26
neha-sharma-geoai <125634000+neha-sharma-geoai@users.noreply.github.com>
2025-07-27 19:41:41 +0000

    Deploying to gh-pages from @ neha-sharma-geoai/neha-sharma-geoai.github.io@2ccfbd341602c84b9d2505cc6dddba707a9a0861 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -580,14 +580,14 @@ ninja.data = [{
           description: "MS Data Science Thesis",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-assessment-of-cropping-patterns-in-districts-of-gujarat",
+          title: 'Assessment of Cropping Patterns in Districts of Gujarat',
+          description: "GIS Foundation",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
+            },},{id: "projects-spatial-livability-index-model-slim-for-rajkot-municipal-corporation",
+          title: 'Spatial Livability Index Model (SLIM) for Rajkot Municipal Corporation',
+          description: "GIS for Governance",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
