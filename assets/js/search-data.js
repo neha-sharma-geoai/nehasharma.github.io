0119cb61dd420c4ece17df14aecb3f0d8e6dffdc
neha-sharma-geoai <125634000+neha-sharma-geoai@users.noreply.github.com>
2025-07-28 21:39:18 +0000

    Deploying to gh-pages from @ neha-sharma-geoai/neha-sharma-geoai.github.io@60c83aa8242c3e766874ba9ccffd8bbcdfa079c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -478,13 +478,13 @@ ninja.data = [{
         },
       },{id: "post-a-post-with-code",
         
-          title: "a post with code",
+          title: 'a post with code <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
         description: "an example of a blog post with some code",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2015/code/";
+            window.open("https://medium.com/@ns_geoai", "_blank");
           
         },
       },{id: "post-a-post-with-images",
