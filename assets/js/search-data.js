9cb30357274bc28731eb2f6c7d68f0d65bebbd4e
neha-sharma-geoai <125634000+neha-sharma-geoai@users.noreply.github.com>
2025-07-28 22:17:49 +0000

    Deploying to gh-pages from @ neha-sharma-geoai/neha-sharma-geoai.github.io@859d855b98a49685e4212b2d5546f3fab69f5bae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -487,17 +487,6 @@ ninja.data = [{
             window.open("https://medium.com/@ns_geoai", "_blank");
           
         },
-      },{id: "post-a-post-with-images",
-        
-          title: "a post with images",
-        
-        description: "this is what included images could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/images/";
-          
-        },
       },{id: "post-a-post-with-formatting-and-links",
         
           title: "a post with formatting and links",
